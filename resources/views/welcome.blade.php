5bc89f57d8c9992bc5ee18ae4ac69f51e8da5d7f
RahmadFirdaus <151610242@smkassalaambandung.sch.id>
2017-08-07 17:17:56 +0700

    Aplikasi Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">Annyeonghaseyo</div>
+        <div class="panel-body">
+           Selamat Datang di Aplikasi Toko Grosir
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
